59cde1d398b1ca1b8a829d1ba9dd167fd4c196d4
yonatanGal <jhonygal@gmail.com>
2021-07-18 19:52:26 +0300

    added users, consts, utils

---
+++ app/src/main/java/com/example/centralbark_PostPc_2021/Consts.java
@@ -0,0 +1,19 @@
+package com.example.centralbark_PostPc_2021;
+
+enum Breed
+{
+    PITBULL,
+    PUG,
+    GOLDEN,
+    LABRADOR,
+    GERMAN_SHEPHERD,
+    MIXED
+}
+
+enum City
+{
+    JERUSALEM,
+    RISHON_LETZION,
+    TEL_AVIV,
+    STREET_DOG
+}
