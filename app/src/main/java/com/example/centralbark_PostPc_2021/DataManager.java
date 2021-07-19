62986ca08c0252bdefcd6f2e0de456bd2c59e8f1
gil.grimberg <gil.grimberg@cs.huji.ac.il>
2021-07-19 11:36:25 +0300

    initialize DataManager with signatures

--- app/src/main/java/com/example/centralbark_PostPc_2021/DataManager.java
+++ app/src/main/java/com/example/centralbark_PostPc_2021/DataManager.java
@@ -9,6 +9,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.FirebaseApp;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class DataManager {
@@ -17,22 +18,43 @@ public class DataManager {
     public SharedPreferences sp;
     public FirebaseApp app;
     public FirebaseFirestore db;
-
+    //todo: add the storage object of firestore
+    private String userId;
     public DataManager(Context context) {
         this.context = context;
         this.sp = context.getSharedPreferences("local_db", Context.MODE_PRIVATE);
-        initializeFromSp();
+        this.userId = initializeFromSp();
+        if (this.userId.equals("noId"))
+        {
+            //todo: go to main sign Up/In screen!
+        }
         app = FirebaseApp.initializeApp(this.context);
         db = FirebaseFirestore.getInstance();
     }
-    private void initializeFromSp() {
-        String userIdStr = this.sp.getString("userId","noId");
-        UUID userId = UUID.fromString(userIdStr);
+    private String initializeFromSp(){
+        return this.sp.getString("userId","noId");
 
     }
     private void updateSp() {
         SharedPreferences.Editor editor = this.sp.edit();
-//        editor.putString("orderId",this.myOrder.orderId);
+        editor.putString("userId",userId);
         editor.apply();
     }
+
+    //todo: delete from sp? from db?
+
+    public void addToPost(Post post){}
+
+    public void addToUsers(User user){}
+
+    public void addToNotifications(Notification notification){}
+
+    public ArrayList<Post> getPosts(){}//todo: return the list of posts related to the user
+
+    public ArrayList<Notification> getNotifications(){}//todo: return the list of posts related to the user
+
+
+
+
+
 }
