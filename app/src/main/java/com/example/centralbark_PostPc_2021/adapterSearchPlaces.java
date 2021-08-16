562fc0c02dfbb0a32fdba1d07a1d1f5bcd613003
lilach41 <lilach.rapaport1@mail.huji.ac.il>
2021-08-16 20:15:29 +0300

    keep working on search places+accounts

--- app/src/main/java/com/example/centralbark_PostPc_2021/adapterSearchPlaces.java
+++ app/src/main/java/com/example/centralbark_PostPc_2021/adapterSearchPlaces.java
@@ -10,17 +10,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.jetbrains.annotations.NotNull;
-import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class adapterSearchPlaces extends RecyclerView.Adapter<adapterSearchPlaces.rowSearchPlacesHolder> {
-    searchPlacesFragment searchPlacesFragment = new searchPlacesFragment();
-    DataManager dataManager;
     Context context;
+    Map<LatLng, String> parks;
+    static ArrayList<LatLng> locations = new ArrayList<>();
+    static ArrayList<String> parksNames = new ArrayList<>();
 
-    public adapterSearchPlaces(Context context, DataManager dataManager){
+
+    public adapterSearchPlaces(Context context,  Map<LatLng, String> parksList){
         this.context = context;
-        this.dataManager = dataManager;
+        this.parks = parksList;
+        for (LatLng location: parks.keySet()){
+            String name = parks.get(location);
+            locations.add(location);
+            parksNames.add(name);
+        }
     }
 
     @NonNull
@@ -34,22 +45,28 @@ public class adapterSearchPlaces extends RecyclerView.Adapter<adapterSearchPlace
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull rowSearchPlacesHolder holder, int position) {
-
-
+        holder.dogParkName.setText(parksNames.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 2;
+        return parks.size();
     }
 
+    public static void filterParks(ArrayList<LatLng> locs, ArrayList<String> parks){
+        locations = locs;
+        parksNames = parks;
+    }
+
+
+
     public class rowSearchPlacesHolder extends RecyclerView.ViewHolder{
-        private TextView dogPark;
+        private TextView dogParkName;
         private Button goNowButton;
 
         public rowSearchPlacesHolder(@NonNull @NotNull View itemView) {
             super(itemView);
-            this.dogPark = itemView.findViewById(R.id.dogParkName_TextView);
+            this.dogParkName = itemView.findViewById(R.id.dogParkName_TextView);
             this.goNowButton = itemView.findViewById(R.id.goNow_Button);
         }
     }
--- app/src/main/java/com/example/centralbark_PostPc_2021/searchPlacesFragment.java
+++ app/src/main/java/com/example/centralbark_PostPc_2021/searchPlacesFragment.java
@@ -8,49 +8,81 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class searchPlacesFragment extends Fragment {
 
     private DataManager dataManager;
-    private FirestoreRecyclerAdapter placesAdapter;
     private RecyclerView placesRecycler;
     Button searchAccountsButton;
     Button searchPlacesButton;
     EditText searchPlacesEditText;
+    adapterSearchPlaces adapter;
 
     public searchPlacesFragment() {
         super(R.layout.fragment_search_places);
     }
 
-    @Nullable
-    @org.jetbrains.annotations.Nullable
+
     @Override
-    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_search_places, container, false);
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        searchAccountsButton = view.findViewById(R.id.searchAccounts_Button_searchPlaces);
+        searchPlacesEditText = view.findViewById(R.id.searchPlaces_EditText);
         placesRecycler = view.findViewById(R.id.recyclerView_SearchPlaces);
-        adapterSearchPlaces adapter = new adapterSearchPlaces(getContext(), dataManager);
+
+        adapter = new adapterSearchPlaces(getContext(), Utils.locationToNameMapping);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         placesRecycler.setLayoutManager(linearLayoutManager);
         placesRecycler.setAdapter(adapter);
-        return view;
+
+        searchAccountsButton.setOnClickListener(v -> {
+            Utils.moveBetweenFragments(R.id.the_screen, new searchAccountFragment(), getActivity(), "search_accounts");
+        });
+
+        searchPlacesEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Map<LatLng, String> filteredMap = new HashMap<>();
+                Map<LatLng, String> parks = Utils.locationToNameMapping;
+                for (LatLng location : parks.keySet()) {
+                    String parkName = parks.get(location);
+                    //if(s.toString().toLowerCase().startsWith(parkName)){
+                    if (parkName.toLowerCase().startsWith(s.toString().toLowerCase())) {
+                        filteredMap.put(location, parkName);
+                    }
+                }
+                adapter = new adapterSearchPlaces(getContext(), filteredMap);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                placesRecycler.setLayoutManager(linearLayoutManager);
+                placesRecycler.setAdapter(adapter);
+            }
+        });
+
+
     }
 
-    @Override
-    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-
-
-    }
 }
