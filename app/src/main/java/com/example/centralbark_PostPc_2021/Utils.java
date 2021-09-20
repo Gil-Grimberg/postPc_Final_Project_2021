b319923db6cc7b47dcc64f8136d94917f24eaa30
lilach41 <lilach.rapaport1@mail.huji.ac.il>
2021-09-20 18:23:46 +0300

    date

--- app/src/main/java/com/example/centralbark_PostPc_2021/Utils.java
+++ app/src/main/java/com/example/centralbark_PostPc_2021/Utils.java
@@ -28,10 +28,10 @@ import java.util.Map;
 public class Utils {
 
     final static List<String> VALID_MONTHS =
-           new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"));
+           new ArrayList<>(Arrays.asList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"));
 
     final static List<String> VALID_DAYS =
-            new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+            new ArrayList<>(Arrays.asList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
             "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
             "29", "30", "31"));
 
