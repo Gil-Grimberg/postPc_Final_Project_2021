cc6f4cad4b7cd2c956e017fb466058e2bd6b604d
Gil-Grimberg <gilgr17.gg@gmail.com>
2021-09-26 13:16:00 +0300

    limit permissions

--- app/src/main/java/com/boss/centralbark_PostPc_2021/AddPostFragment.java
+++ app/src/main/java/com/boss/centralbark_PostPc_2021/AddPostFragment.java
@@ -2,6 +2,8 @@ package com.boss.centralbark_PostPc_2021;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -16,15 +18,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
 import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -41,15 +47,15 @@ public class AddPostFragment extends Fragment {
     User myUser;
     private final int STORAGE_PERMISSION_CODE = 1;
 
-    public AddPostFragment(){
+    public AddPostFragment() {
         super(R.layout.fragment_add_post);
-        if(dataManager ==null){
+        if (dataManager == null) {
             this.dataManager = CentralBarkApp.getInstance().getDataManager();
         }
     }
 
     @Override
-    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState){
+    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.uploadIm = view.findViewById(R.id.upload_im_add_post_screen);
         this.caption = view.findViewById(R.id.caption_text_add_post_screen);
@@ -57,19 +63,16 @@ public class AddPostFragment extends Fragment {
 
         // upload image from phone method:
         ActivityResultLauncher<Intent> upLoadLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultContracts.StartActivityForResult(),
                 result -> {
-                    if(result.getResultCode()== Activity.RESULT_OK){
+                    if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
                         assert data != null;
                         Uri selectedImage = data.getData();
-                        if (selectedImage == null)
-                        {
+                        if (selectedImage == null) {
                             Toast.makeText(getContext(), "Error uploading image", Toast.LENGTH_LONG).show();
-                        }
-                        else
-                        {
-                            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                        } else {
+                            String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
                             Cursor cursor = getActivity().getContentResolver().query(selectedImage,
                                     filePathColumn, null, null, null);
@@ -77,9 +80,9 @@ public class AddPostFragment extends Fragment {
                             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                             picturePath = cursor.getString(columnIndex);
                             String[] picturePathSplit = picturePath.split("\\.");
-                            fileType = picturePathSplit[picturePathSplit.length-1];
+                            fileType = picturePathSplit[picturePathSplit.length - 1];
                             image = new File(picturePath);
-                            if(image.exists()){
+                            if (image.exists()) {
                                 uploadIm.setImageURI(Uri.fromFile(image));
                             }
                             cursor.close();
@@ -91,19 +94,35 @@ public class AddPostFragment extends Fragment {
         // upload photo button
         this.uploadIm.setOnClickListener(v -> {
             try {
-                if (!(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED))
-                {
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-                }
-                else
-                {
+                if (!(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        new AlertDialog.Builder(getContext())
+                                .setTitle("Permission needed")
+                                .setMessage("This permission is needed because of this and that")
+                                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        ActivityCompat.requestPermissions(getActivity(),
+                                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                                    }
+                                })
+                                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .create().show();
+                    } else {
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                    }
+                } else {
                     Intent uploadIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    upLoadLauncher.launch(uploadIntent); }
-            }
-            catch (Exception e)
-            {
-                Toast.makeText(getContext(),"problem with media permissions",Toast.LENGTH_LONG).show();
+                    upLoadLauncher.launch(uploadIntent);
+                }
+            } catch (Exception e) {
+                Toast.makeText(getContext(), "problem with media permissions", Toast.LENGTH_LONG).show();
             }
 
         });
@@ -111,15 +130,14 @@ public class AddPostFragment extends Fragment {
 
         // upload post button
         this.uploadPostButton.setOnClickListener(v -> {
-            if(picturePath == null){
+            if (picturePath == null) {
                 Toast.makeText(getContext(), "You need to chose an image to post!", Toast.LENGTH_LONG).show();
-            }
-            else{
+            } else {
                 String myId = this.dataManager.getMyId();
                 this.dataManager.db.collection("Users").document(myId).get().addOnSuccessListener(documentSnapshot -> {
                     myUser = documentSnapshot.toObject(User.class);
                     String postId = UUID.randomUUID().toString();
-                    String postPath = "post_photos/"+postId+"."+fileType;
+                    String postPath = "post_photos/" + postId + "." + fileType;
                     String content = caption.getText().toString();
                     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     String curDateString = sdf.format(new Date());
@@ -128,7 +146,7 @@ public class AddPostFragment extends Fragment {
                     StorageReference imgRef = storageReference.child(postPath);
                     UploadTask uploadTask = imgRef.putFile(Uri.fromFile(new File(picturePath)));
                     uploadTask.addOnSuccessListener(taskSnapshot -> {
-                        Post curPost = new Post(myId,postId,myUser.getUsername(),myUser.getProfilePhoto(),postPath, content, 0, curDateString, myUser.getFriendList());
+                        Post curPost = new Post(myId, postId, myUser.getUsername(), myUser.getProfilePhoto(), postPath, content, 0, curDateString, myUser.getFriendList());
                         dataManager.addToPost(curPost);
                         Utils.moveBetweenFragments(R.id.the_screen, new FeedFragment(), getActivity(), "feed");
 
